6f9e85272c02b61f05513f5006764b68e18d4355
Mariusz Topolewski <mariaszek9003@gmial.com>
2018-06-17 12:45:11 +0200

    Added implementation of NFC module in app

--- AndroidNFCSampleApp/AndroidNFCSampleApp/MainActivity.cs
+++ AndroidNFCSampleApp/AndroidNFCSampleApp/MainActivity.cs
@@ -1,19 +1,55 @@
-﻿using Android.App;
-using Android.Widget;
-using Android.OS;
-using Android.Support.V7.App;
+﻿using Android.OS;
+using Android.App;
+using Android.Nfc;
+using Android.Content;
 
 namespace AndroidNFCSampleApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
+        #region Fields
+        private NfcAdapter nfcAdapter;
+        #endregion
+
+        #region LifeCycle
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             SetContentView(Resource.Layout.activity_main);
+
+            nfcAdapter = NfcAdapter.GetDefaultAdapter(this);
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            if (nfcAdapter == null)
+            {
+                var alert = new AlertDialog.Builder(this).Create();
+                alert.SetMessage("NFC is not supported on this device.");
+                alert.SetTitle("NFC Unavailable");
+                alert.Show();
+            }
+            else
+            {
+                var tagDetected = new IntentFilter(NfcAdapter.ActionTagDiscovered);
+                var filters = new[] { tagDetected };
+                var intent = new Intent(this, this.GetType()).AddFlags(ActivityFlags.SingleTop);
+                var pendingIntent = PendingIntent.GetActivity(this, 0, intent, 0);
+
+                nfcAdapter.EnableForegroundDispatch(this, pendingIntent, filters, null);
+            }
+        }
+        #endregion
+
+        #region Actions
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+        }
+        #endregion
     }
 }
 
